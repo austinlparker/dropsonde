001048c90d5dea3515d5b0b5d6eecebd1c70d503
austinlparker <austin@ap2.io>
2024-04-13 21:41:28 -0400

    updates

---
+++ ui/model/trace.go
@@ -0,0 +1,26 @@
+package model
+
+import "go.opentelemetry.io/collector/pdata/ptrace"
+
+func ParseTraceMessage(msg WSMessage) (ptrace.Traces, error) {
+	unmarshaler := &ptrace.JSONUnmarshaler{}
+	traces, err := unmarshaler.UnmarshalTraces(msg.data)
+	if err != nil {
+		return traces, err
+	}
+	return traces, nil
+}
+
+func AddToSpanList(traces ptrace.Traces, list []ptrace.Span) []ptrace.Span {
+	for i := 0; i < traces.ResourceSpans().Len(); i++ {
+		rs := traces.ResourceSpans().At(i)
+		for j := 0; j < rs.ScopeSpans().Len(); j++ {
+			ss := rs.ScopeSpans().At(j)
+			for k := 0; k < ss.Spans().Len(); k++ {
+				span := ss.Spans().At(k)
+				list = append(list, span)
+			}
+		}
+	}
+	return list
+}
